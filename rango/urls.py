186d6daf84b2a38cdd34d13f076ff05ed7c3310d
Aung Naing Oo <au.naingoo@gmail.com>
2017-02-21 16:14:28 +0800

    First commit

---
+++ rango/urls.py
@@ -0,0 +1,8 @@
+from rango import views
+from django.conf.urls import url
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+]
---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hello to the world")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page")
