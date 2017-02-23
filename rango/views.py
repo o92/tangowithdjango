4a305a7a0d195666dae64d730ee5d2d874007d46
Aung Naing Oo <au.naingoo@gmail.com>
2017-02-23 15:17:27 +0800

    Added Models

--- rango/views.py
+++ rango/views.py
@@ -11,11 +11,9 @@ def about(request):
 def indexHtml(request):
     #Construct a dictionary to pass to the template engine as its context
     #Note the key boldmessage is the same as {{boldmessage}} in the template
-
     context_dict = {'boldmessage':"I am bold font from the context"}
 
     #return a rendered response to send to the client
     #we make use of the shortcut function to make our lives easier
     #Note that the first parameter is the template we wish to use
-
     return render(request, 'rango/index.html',context_dict)
--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['192.168.0.108']
 
 #Path to templates
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 # Application definition
 
@@ -125,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   STATIC_PATH,
+]
